3b08b953cc3059a325c0e745b37b26cdc9d4e9d3
Chase Coleman <167048137+colemchase@users.noreply.github.com>
2026-07-04 16:51:30 -0400

    Add: final-array-state-after-k-multiplication-operations-i - submission-0

---
+++ Data Structures & Algorithms/final-array-state-after-k-multiplication-operations-i/submission-0.py
@@ -0,0 +1,15 @@
+class Solution:
+    def getFinalState(self, nums: List[int], k: int, multiplier: int) -> List[int]:
+        nums = [(num, i) for i, num in enumerate(nums)]
+        heapq.heapify(nums)
+        # apply the operation to the min and push it, do this k times
+        for _ in range(k):
+            num, i = heapq.heappop(nums)
+            heapq.heappush(nums, (num*multiplier, i))
+        # turn back into list and return
+        res = [0 for _ in range(len(nums))]
+
+        for num, i in nums:
+            res[i] = num
+
+        return res
