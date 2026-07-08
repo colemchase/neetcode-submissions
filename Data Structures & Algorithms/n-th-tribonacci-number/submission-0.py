75367b65aac08752cc8f8d7a3760671750a69236
Chase Coleman <167048137+colemchase@users.noreply.github.com>
2026-07-08 18:30:55 -0400

    Add: n-th-tribonacci-number - submission-0

---
+++ Data Structures & Algorithms/n-th-tribonacci-number/submission-0.py
@@ -0,0 +1,8 @@
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        trib = [0, 1, 1]
+
+        while len(trib) <= n:
+            trib.append(trib[-1] + trib[-2] + trib[-3])
+        
+        return trib[n]
