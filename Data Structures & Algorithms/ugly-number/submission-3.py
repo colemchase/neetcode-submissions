4540ea7c06d6d68281d84920754db0c0e98ba68a
Chase Coleman <167048137+colemchase@users.noreply.github.com>
2026-07-10 10:06:46 -0400

    Add: ugly-number - submission-3

---
+++ Data Structures & Algorithms/ugly-number/submission-3.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isUgly(self, n: int) -> bool:
+        while n > 1:
+            if n % 2 == 0:
+                n /= 2
+                continue
+            if n % 3 == 0:
+                n /= 3
+                continue
+            if n % 5 == 0:
+                n /= 5
+                continue
+            return False
+        return n == 1
+  
+
