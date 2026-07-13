608551e0dc67810af9176cca0e8ab2e068c60a1c
Chase Coleman <167048137+colemchase@users.noreply.github.com>
2026-07-13 17:34:39 -0400

    Add: longest-turbulent-subarray - submission-2

---
+++ Data Structures & Algorithms/longest-turbulent-subarray/submission-2.py
@@ -0,0 +1,31 @@
+class Solution:
+    def maxTurbulenceSize(self, arr: List[int]) -> int:
+        res = 0
+
+        memo = {}
+        def turb(i, up):
+            if i < len(arr) - 1:
+                if (i, up) in memo:
+                    return memo[(i,up)]
+                if up and arr[i] < arr[i+1]: # up
+                    return 1 + turb(i+1, 0)
+                if not up and arr[i] > arr[i+1]: # down
+                    return 1 + turb(i+1, 1)
+            return 1
+
+
+    
+        
+        res = 0
+    
+        def dive(i):
+            if i < len(arr):
+                nonlocal res
+                down = turb(i, 0)
+                up = turb(i, 1)
+                res = max(res, down, up)  
+                dive(i+up)
+                dive(i+down)
+        dive(0)
+        
+        return res
