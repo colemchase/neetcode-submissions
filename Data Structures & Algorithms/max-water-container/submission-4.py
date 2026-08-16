4e63c13a9a0e2e8dd40de5c846222d1c95d9889e
Chase Coleman <167048137+colemchase@users.noreply.github.com>
2026-08-16 18:55:49 -0400

    Add: max-water-container - submission-4

---
+++ Data Structures & Algorithms/max-water-container/submission-4.py
@@ -0,0 +1,33 @@
+class Solution:
+    def maxArea(self, heights: List[int]) -> int:
+        
+        # Brutal
+        # double for each different contianer possible
+        # res = 0
+        # for i in range(len(heights)):
+        #     for j in range(i+1, len(heights)):
+        #         h = min(heights[i], heights[j])
+        #         w = j-i
+        #         res = max(res, h * w)
+
+        # return res
+
+        # Optimal
+        # Two pointer 
+        res = 0
+        l = 0
+        r = len(heights)-1
+        while l < r:
+            h = min(heights[l], heights[r])
+            w = r - l
+            res = max(res, h * w)
+
+            ### decide how to make w smaller
+            # chose smaller outer height
+            if heights[l] < heights[r]:
+                l+=1
+            else:
+                r-=1
+        
+        return res
+
